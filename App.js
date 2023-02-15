631f52800af43251db1e5a329c8bf0cf1517d712
Muhammad Ameen <mameen@nisum.com>
2023-02-15 12:02:06 +0500

    Style changes

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
-import HelloWorld from "./src/components/HelloWorld";
 import SplashScreen from "react-native-splash-screen";
 import Signup from "./src/screens/signup/Signup";
 import Login from "./src/screens/login/Login";
+import Home from "./src/screens/home/Home";
 
 export default function App() {
   useEffect(() => {
@@ -10,7 +10,8 @@ export default function App() {
   }, []);
   const [isLogin, setIsLogin] = useState(true);
   return isLogin ? (
-    <Login setIsLogin={setIsLogin} />
+    // <Login setIsLogin={setIsLogin} />
+    <Home />
   ) : (
     <Signup setIsLogin={setIsLogin} />
   );
---
+++ src/constants/mock.js
@@ -0,0 +1,109 @@
+export const medicineArr = [
+  {
+    id: 0,
+    image:
+      "https://i-cf65.ch-static.com/content/dam/cf-consumer-healthcare/panadol/en_pk/pakistan_product/panadol-regular/408x300-panadol-regular.png?auto=format",
+    name: "Panadol",
+    price: 18.0,
+  },
+  {
+    id: 1,
+    image:
+      "https://www.dawailo.pk/wp-content/uploads/2021/07/Screenshot1-6.png",
+    name: "Amoxil 125mg syrup",
+    price: 128.0,
+  },
+  {
+    id: 2,
+    image: "https://www.emeds.pk//upload/pro-imges/image-1/arinac-forte.jpg",
+    name: "Arinac Forte",
+    price: 50.0,
+  },
+  {
+    id: 3,
+    image:
+      "https://www.telfast.com/dam/jcr:5e684a80-3e87-4fda-8300-8423ad32cd99/Telfast_120.png",
+    name: "Telfast 120mg",
+    price: 75.0,
+  },
+  {
+    id: 4,
+    image: "https://www.almishkat.pk/wp-content/uploads/2021/01/566pics.jpg",
+    name: "Risek 40mg",
+    price: 120.0,
+  },
+  {
+    id: 5,
+    image:
+      "https://media.naheed.pk/catalog/product/cache/17cb4734a0ff40d859ea4c0cfd876903/1/0/1056474-1.jpg",
+    name: "Coferb Cough Syrup",
+    price: 90.0,
+  },
+  {
+    id: 6,
+    image:
+      "https://i-cf65.ch-static.com/content/dam/cf-consumer-healthcare/panadol/en_ke/products/455x455/Panadol-Extra-100s-408x300.png?auto=format",
+    name: "Panadol Extra",
+    price: 90.0,
+  },
+  {
+    id: 7,
+    image: "https://products.dawaai.pk/800/cclval13219_1650343053_1.webp",
+    name: "Valam-H 5/180mg",
+    price: 90.0,
+  },
+  {
+    id: 8,
+    image:
+      "https://cdn.shopify.com/s/files/1/0088/4758/9476/products/07716_1200x1349.jpg?v=1596438884",
+    name: "Amoxil 120mg",
+    price: 90.0,
+  },
+  {
+    id: 9,
+    image:
+      "https://pillintrip.com/style/images/medicines/augmentan/augmentan1.jpg",
+    name: "Augmantan 120mg",
+    price: 90.0,
+  },
+  {
+    id: 10,
+    image:
+      "https://www.dawailo.pk/wp-content/uploads/2021/07/Screenshot1-6.png",
+    name: "Amoxil 125mg syrup",
+    price: 128.0,
+  },
+  {
+    id: 11,
+    image: "https://www.emeds.pk//upload/pro-imges/image-1/arinac-forte.jpg",
+    name: "Arinac Forte",
+    price: 50.0,
+  },
+  {
+    id: 12,
+    image:
+      "https://www.telfast.com/dam/jcr:5e684a80-3e87-4fda-8300-8423ad32cd99/Telfast_120.png",
+    name: "Telfast 120mg",
+    price: 75.0,
+  },
+  {
+    id: 13,
+    image: "https://www.almishkat.pk/wp-content/uploads/2021/01/566pics.jpg",
+    name: "Risek 40mg",
+    price: 120.0,
+  },
+  {
+    id: 14,
+    image:
+      "https://cdn.shopify.com/s/files/1/0088/4758/9476/products/07716_1200x1349.jpg?v=1596438884",
+    name: "Amoxil 125mg syrup",
+    price: 90.0,
+  },
+  {
+    id: 15,
+    image:
+      "https://pillintrip.com/style/images/medicines/augmentan/augmentan1.jpg",
+    name: "Augmantan 120mg",
+    price: 90.0,
+  },
+];
